b24c05262f7bc476adb9e27481548778f7c1b804
Umar Hussain <ufhussai@uwaterloo.ca>
2022-07-14 14:18:39 -0400

    minor syntax change to make threshold a control var

--- src/threshold_detect.py
+++ src/threshold_detect.py
@@ -4,6 +4,7 @@ from scipy import ndimage
 import matplotlib.pyplot as plt
 
 filename = "match_data/data9.csv"
+temperature_threshold = 36
 
 thermal_data = np.loadtxt(filename, delimiter=",")
 thermal_data[6][0] = (thermal_data[6][1] + thermal_data[5][0] + thermal_data[7][0]) / 3
@@ -20,7 +21,7 @@ data_array = np.reshape(thermal_data, mlx_shape)  # reshape
 data_array = np.flipud(data_array)  # mirror image
 data_array = ndimage.zoom(data_array, mlx_interp_val)  # interpolate
 
-num_over_thresh = np.count_nonzero(data_array > 36)
+num_over_thresh = np.count_nonzero(data_array > temperature_threshold)
 print("num_over_thresh: ", num_over_thresh)
 if num_over_thresh:
     print("fire detected! :(")
@@ -34,6 +35,7 @@ plt.imshow(
     vmin=np.min(data_array),
     vmax=np.max(data_array),
 )
+
 plt.colorbar()  # setup colorbar
 # plt.set_label("Temperature [$^{\circ}$C]", fontsize=14)  # colorbar label
 plt.show()

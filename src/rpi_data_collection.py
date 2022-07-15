9fa6c60cf98fcdc4d5be9da2f5db46f869b41204
Daniel Arakcheev <darakche@uwaterloo.ca>
2022-07-15 15:45:57 -0400

    working on db uploading

--- src/rpi_data_collection.py
+++ src/rpi_data_collection.py
@@ -7,6 +7,8 @@ import numpy as np
 import io
 import socket
 import time
+from PIL import Image
+import os
 
 DEBUG = True
 
@@ -17,11 +19,12 @@ if not DEBUG:
     from picamera import PiCamera
     import board
 
-class DataCollector:
 
+class DataCollector:
     def __init__(self):
         # Load GPS coordinates from mission planner.
-        self.gps_path = "..\path_planning\path_test.waypoints"
+        self.gps_path = os.path.join("..", "path_planning", "path_test.waypoints")
+
         self.gps_coordinates = self.load_gps(self.gps_path)
         self.num_pics = len(self.gps_coordinates)
 
@@ -32,25 +35,26 @@ class DataCollector:
         self.setupSensors()
         self.flightDataCollection()
 
-
     def setupSensors(self):
         # Instantiate sensor modules & communication protocol
         if not DEBUG:
-            i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
-            self.mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
-            self.mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ # set refresh rate
+            i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)  # setup I2C
+            self.mlx = adafruit_mlx90640.MLX90640(i2c)  # begin MLX90640 with I2C comm
+            self.mlx.refresh_rate = (
+                adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
+            )  # set refresh rate
             self.camera = PiCamera()
-            self.camera.resolution = (320,240)
+            self.camera.resolution = (320, 240)
             self.camera.start_preview()
-            
-        self.mlx_shape = (24,32)
+
+        self.mlx_shape = (24, 32)
 
     def encode_frame(self, frame):
         f = io.BytesIO()
         np.savez(f, frame=frame)
-        
+
         packet_size = len(f.getvalue())
-        header = '{0}:'.format(packet_size)
+        header = "{0}:".format(packet_size)
         header = bytes(header.encode())  # prepend length of array
 
         out = bytearray()
@@ -74,37 +78,41 @@ class DataCollector:
             raise
 
     def load_gps(self, path):
-        ''' Waypoint file format for parsing
-            QGC WPL <VERSION>
-            <INDEX> <CURRENT WP> <COORD FRAME> <COMMAND> <PARAM1> <PARAM2> <PARAM3> <PARAM4> <PARAM5/X/LATITUDE> <PARAM6/Y/LONGITUDE> <PARAM7/Z/ALTITUDE> <AUTOCONTINUE>
-        '''
+        """Waypoint file format for parsing
+        QGC WPL <VERSION>
+        <INDEX> <CURRENT WP> <COORD FRAME> <COMMAND> <PARAM1> <PARAM2> <PARAM3> <PARAM4> <PARAM5/X/LATITUDE> <PARAM6/Y/LONGITUDE> <PARAM7/Z/ALTITUDE> <AUTOCONTINUE>
+        """
         file = open(path, "r")
-        file.readline() # Read QGC, WPL <Version>
-        file.readline() # Read home location information (not a waypoint)
-        path_data = file.readlines() # path_data contains all the waypoints
+        file.readline()  # Read QGC, WPL <Version>
+        file.readline()  # Read home location information (not a waypoint)
+        path_data = file.readlines()  # path_data contains all the waypoints
         gps_coord = []
-        
+
         # Parse GPS coordinates for all the waypoints
         for wayPoint in path_data:
-            wayPoint = wayPoint.split('\t')
+            wayPoint = wayPoint.split("\t")
             gps_coord.append((wayPoint[8], wayPoint[9]))
 
         return gps_coord
 
     def get_curr_gps(self):
-        return np.array([0,0])
-    
+        x = np.random.uniform(-10, 10)
+        y = np.random.uniform(-10, 10)
+        return np.array([6, 9])
+
     def flightDataCollection(self):
         flightNum = 1
-        while(True):
+        while True:
             print(f"flight #{flightNum} =============================================")
             sensor_data = self.collectPhotosOnFlight()
             self.startClient()
             self.sendData(sensor_data)
-            self.clientSocket.send(b'e')
+            self.clientSocket.send(b"e")
             self.clientSocket.close()
             flightNum += 1
-            time.sleep(10)
+            if flightNum > 1:
+                break
+            time.sleep(5)
 
     def collectPhotosOnFlight(self):
         print("collecting photos")
@@ -113,31 +121,32 @@ class DataCollector:
         num = self.num_pics if not DEBUG else 1
 
         # Collect data
-        while(len(sensor_data) < num):
-            curr_coord = self.get_curr_gps() # Current GPS position recieved over telemetary port from drone
+        while len(sensor_data) < num:
+            curr_coord = (
+                self.get_curr_gps()
+            )  # Current GPS position recieved over telemetary port from drone
             target_coord = self.gps_coordinates[gps_id]
             time.sleep(2)
             # Check to see if current position is within range of next waypoint
             # if abs(curr_coord[0] - target_coord[0]) < 1.5 and abs(curr_coord[1] - target_coord[1]) < 1.5:
             try:
                 curr_time = np.array([datetime.now()])
-                frame = np.zeros((24*32))
-                
-                if DEBUG:
-                    frame = np.random.uniform(-20.0, 200.0, 32*24)
-                else:
-                    mlx.getFrame(frame)                    
+                frame = np.zeros((24 * 32))
 
-                ir_data = (np.reshape(frame, self.mlx_shape))
+                if DEBUG:
+                    frame = np.random.uniform(-20.0, 200.0, 32 * 24)
+                else:
+                    mlx.getFrame(frame)
+
+                ir_data = np.reshape(frame, self.mlx_shape)
 
                 # Get image data as numpy array
-                img_data = np.empty((240*320*3), dtype=np.uint8)
-
+                img_data = np.zeros((240, 320, 3), dtype=np.uint8)
                 if DEBUG:
-                    img_data = np.reshape(img_data, (240,320,3))
+                    img_data[0:100, 0:100] = [255, 0, 255]
                 else:
-                    camera.capture(img_data, 'bgr')
-                
+                    camera.capture(img_data, "bgr")
+
                 sensor_data.append((ir_data, img_data, curr_coord, curr_time))
                 gps_id += 1
 
@@ -149,7 +158,7 @@ class DataCollector:
     def startClient(self):
         # Poll for socket connection
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        while (1):
+        while 1:
             try:
                 self.clientSocket.connect((self.server_addr, 2022))
                 break
--- src/uploadNewData.py
+++ src/uploadNewData.py
@@ -1,3 +1,4 @@
+from re import A
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
@@ -6,66 +7,63 @@ import numpy as np
 from io import BytesIO
 import logging
 import io
+from PIL import Image
+import datetime
+import os
+
 
 class DataUploader:
-
     def __init__(self):
         server_addr = "192.168.10.43"
         self.main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_addr = 0
         self.client_conn = 0
         self.socket_buffer_size = 1024
-        self.mlx_shape = (32,24)
+        self.mlx_shape = (32, 24)
+        self.rgb_shape = (240, 320, 3)
         self.frameCount = 1
 
         self.frameBuffer = bytearray()
-        self.states = ["ir","rgb","gps","time"]
+        self.states = ["ir", "rgb", "gps", "time"]
         self.currState = 0
 
         self.allData = []
         self.tempList = []
 
+        self.rgb_folder_path = "..\server_hd\rgb_images"
+        self.ir_folder_path = "..\server_hd\ir_images"
+
         self.pw = "MySQL1738!"
-        self.create_SQL_connection("localhost", "root", self.pw)
-        
-        self.startServer()     
+        self.sqlConn = create_db_connection("localhost", "root", self.pw, "firefly")
+
+        self.startServer()
         self.acceptDroneConnections()
-        
-
-    def create_SQL_connection(self, host_name, user_name, user_password):
-        connection = None
-        try:
-            connection = mysql.connector.connect(
-                host=host_name,
-                user=user_name,
-                passwd=user_password
-            )
-            print("MySQL Database connection successful")
-        except Error as err:
-            print(f"Error: '{err}'")
-
-        return connection
 
     def startServer(self):
         # host=socket.gethostname()
-        host = '127.0.0.1'
-        ip=socket.gethostbyname(host)
+        host = "127.0.0.1"
+        ip = socket.gethostbyname(host)
         port = 2022
 
         self.main_socket.bind((host, port))
         self.main_socket.listen()
 
     def acceptDroneConnections(self):
+        acceptedCount = 1
         while True:
             print("waiting for a connection")
             self.client_conn, self.client_addr = self.main_socket.accept()
             print(f"connected to: {self.client_addr[0]}")
-            
+
             while True:
                 self.receiveFrame()
                 if self.closeSocketFlag:
                     self.frameBuffer = bytearray()
                     break
+
+            acceptedCount += 1
+            if acceptedCount > 1:
+                break
 
     def receiveFrame(self):
         bufferToProcess = None
@@ -74,7 +72,7 @@ class DataUploader:
 
         while True:
             data = self.client_conn.recv(self.socket_buffer_size)
-            if self.frameBuffer == b'e':
+            if self.frameBuffer == b"e":
                 self.closeSocketFlag = True
                 return
 
@@ -84,19 +82,19 @@ class DataUploader:
                 # if full frame received truncate and proceed to processing
                 bufferToProcess = self.frameBuffer[:length]
                 self.frameBuffer = self.frameBuffer[length:]
-                #print("received full frame... breaking out")
+                # print("received full frame... breaking out")
                 break
-            
+
             if length is None:
                 # havent gotten a packet yet
-                if b':' in self.frameBuffer:
+                if b":" in self.frameBuffer:
                     # this packet is the first one in the msg
                     # remove the length bytes from the front of frameBuffer
                     # leave any remaining bytes in the frameBuffer!
-                    length_str, _, self.frameBuffer = self.frameBuffer.partition(b':')
+                    length_str, _, self.frameBuffer = self.frameBuffer.partition(b":")
                     length = int(length_str)
                     print(f"length of {self.states[self.currState]}: {length}")
-                    
+
                     # the message is entirely in this packet, therefore cut off remaining bytes
                     if len(self.frameBuffer) > length:
                         # split off the full message from the remaining bytes
@@ -105,20 +103,17 @@ class DataUploader:
                         self.frameBuffer = self.frameBuffer[length:]
                         self.length = None
                         # break out of receiving loop and start processing
-                        #print("frame smaller than buffer")
+                        # print("frame smaller than buffer")
                         break
                 else:
-                    #print(len(self.frameBuffer))
+                    # print(len(self.frameBuffer))
                     continue
 
-        #print(f"final length of frameBuffer: {len(bufferToProcess)}")
-        frame = np.load(io.BytesIO(bufferToProcess), allow_pickle=True)['frame']
-        self.processData(frame)
+        # print(f"final length of frameBuffer: {len(bufferToProcess)}")
+        frame = np.load(io.BytesIO(bufferToProcess), allow_pickle=True)["frame"]
+        self.saveFrame(frame)
 
-    def processData(self, frame): 
-        #reshaped = np.reshape(frame, self.mlx_shape)
-        #print(frame)
-
+    def saveFrame(self, frame):
         print(f"processed incoming frame #{self.frameCount}")
         print(f"{self.states[self.currState]} frame received")
         print("")
@@ -126,6 +121,7 @@ class DataUploader:
         self.tempList.append(frame)
 
         if self.currState == 3:
+            self.processData(self.tempList)
             self.allData.append(self.tempList)
             self.tempList = []
             print(f"size of alldata : {len(self.allData)}")
@@ -134,13 +130,84 @@ class DataUploader:
         self.currState = (self.currState + 1) % 4
         self.frameCount += 1
 
-    def appendImageToDB(self):
-        print("running sql query")
-        # run sql query
+    def processData(self, dataList):
+        print(f"processing frame #{self.frameCount}")
+        irRaw, rgbRaw, gpsRaw, timeRaw = dataList
+        # ir_data = (np.reshape(irRaw, self.mlx_shape))
+        # rgb_data = (np.reshape(rgbRaw, self.rgb_shape))
+        lon = gpsRaw[0]
+        lat = gpsRaw[1]
+        datetime_object = timeRaw[0]
+        date = datetime_object.strftime("%d-%m-%Y")
+        gps_time_part = f"{lon}_{lat}__{date}"
+
+        # save ir and rgb images as PNGs
+        ir_file_path = self.saveArrToPNG(irRaw, gps_time_part, type="ir")
+        rgb_file_path = self.saveArrToPNG(rgbRaw, gps_time_part, type="rgb")
+
+        self.appendEntryToDB(lon, lat, datetime_object, ir_file_path, rgb_file_path)
+
+    def saveArrToPNG(self, rawData, extension, type):
+        file_name = f"{type}_{extension}.png"
+        file_path = os.path.join("..", "server_hd", f"{type}_images", file_name)
+        im = Image.fromarray(rawData, "L" if type == "ir" else "RGB")
+        im.save(file_path)
+        print(f"saved {type} as png")
+        return file_path
+
+    def appendEntryToDB(self, lon, lat, date, ir_path, rgb_path):
+        print("adding entry to db")
+
+        # check if location exists in locations table
+        query_checkLoc = (
+            f"SELECT locID FROM locations WHERE lon = {lon} and lat = {lat};"
+        )
+        result = read_query(self.sqlConn, query_checkLoc)
+        if not result:
+            # make a location record
+            query_addLoc = f"INSERT INTO locations (lon, lat) VALUES ({lon}, {lat});"
+            execute_query(self.sqlConn, query_addLoc)
+            result = read_query(self.sqlConn, query_checkLoc)
+
+        locID = result[0][0]
+
+        query = f"INSERT INTO image_records (locID, datetime, irImagePath, rgbImagePath) VALUES ({locID}, {date}, {ir_path}, {rgb_path});"
+        print(query)
+        execute_query(self.sqlConn, query)
+
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name, user=user_name, passwd=user_password, database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error: '{err}'")
 
 
 d1 = DataUploader()
-
-
-
-

11485a044ed5221a7076f9b6eb5c62bddd0530a5
Daniel Arakcheev <darakche@uwaterloo.ca>
2022-07-14 20:17:39 -0400

    refactored client code

--- src/rpi_data_collection.py
+++ src/rpi_data_collection.py
@@ -3,123 +3,148 @@
 ##########################################
 
 from datetime import datetime
-import board
-import busio
 import numpy as np
-import adafruit_mlx90640
 import io
 import socket
-from picamera import PiCamera
 import time
 
+USING_RASPI = False
 
-def encode_frame(frame):
-    f = io.BytesIO()
-    np.savez(f, frame=frame)
-    
-    packet_size = len(f.getvalue())
-    header = '{0}:'.format(packet_size)
-    header = bytes(header.encode())  # prepend length of array
+# imports for raspberry pi
+if USING_RASPI:
+    import busio
+    import adafruit_mlx90640
+    from picamera import PiCamera
+    import board
 
-    out = bytearray()
-    out += header
+class DataCollector:
 
-    f.seek(0)
-    out += f.read()
-    return out
+    def __init__(self):
+        # Load GPS coordinates from mission planner.
+        self.gps_path = "../path_plannin/path_test.waypoints"
+        self.gps_coordinates = self.load_gps(self.gps_path)
+        self.num_pics = len(self.gps_coordinates)
 
-def send(frame, socket):
-    if not isinstance(frame, np.ndarray):
-        raise TypeError("input frame is not a valid numpy array")
+        #self.server_addr = "192.168.10.43"
+        self.server_addr = "127.0.0.1"
 
-    out = encode_frame(frame)
-
-    try:
-        socket.sendall(out)
-    except BrokenPipeError:
-        print("connection broken")
-        raise
-
-def load_gps(path):
-    ''' Waypoint file format for parsing
-        QGC WPL <VERSION>
-        <INDEX> <CURRENT WP> <COORD FRAME> <COMMAND> <PARAM1> <PARAM2> <PARAM3> <PARAM4> <PARAM5/X/LATITUDE> <PARAM6/Y/LONGITUDE> <PARAM7/Z/ALTITUDE> <AUTOCONTINUE>
-    '''
-    file = open(path, "r")
-    file.readline() # Read QGC, WPL <Version>
-    file.readline() # Read home location information (not a waypoint)
-    path_data = file.readlines() # path_data contains all the waypoints
-    gps_coord = []
-    
-    # Parse GPS coordinates for all the waypoints
-    for wayPoint in path_data:
-        wayPoint = wayPoint.split('\t')
-        gps_coord.append((wayPoint[8], wayPoint[9]))
-
-    return gps_coord
-
-# Return np array
-def get_curr_gps():
-    return np.array([0,0])
+        self.setupSensors()
+        self.flightDataCollection()
 
 
-# Load GPS coordinates from mission planner.
-gps_path = "..\path_planning\path_test.waypoints"
-gps_coordinates = load_gps(gps_path)
-num_pics = len(gps_coordinates)
-
-# Instantiate sensor modules & communication protocol
-i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
-mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ # set refresh rate
-mlx_shape = (24,32)
-
-camera = PiCamera()
-camera.resolution = (720,480)
-camera.start_preview()
-
-server_addr = "192.168.10.43"
-while(1):
-    sensor_data = []
-    gps_id = 0
-
-    # Collect data
-    while(len(sensor_data) < num_pics):
-        curr_coord = get_curr_gps() # Current GPS position recieved over telemetary port from drone
-        target_coord = gps_coordinates[gps_id]
-        time.sleep(5)
-        # Check to see if current position is within range of next waypoint
-        # if abs(curr_coord[0] - target_coord[0]) < 1.5 and abs(curr_coord[1] - target_coord[1]) < 1.5:
-        try:
-            curr_time = np.array([datetime.now()])
-            frame = np.zeros((24*32))
-            mlx.getFrame(frame)
-            ir_data = (np.reshape(frame,mlx_shape))
-
-            # Get image data as numpy array
-            img_data = np.empty((480*720*3), dtype=np.uint8)
-            camera.capture(img_data, 'bgr')
-            img_data = np.reshape(img_data, (480,720,3))
+    def setupSensors(self):
+        # Instantiate sensor modules & communication protocol
+        if USING_RASPI:
+            i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
+            self.mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
+            self.mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ # set refresh rate
+            self.camera = PiCamera()
+            self.camera.resolution = (320,240)
+            self.camera.start_preview()
             
-            sensor_data.append((ir_data, img_data, curr_coord, curr_time))
-            gps_id += 1
+        self.mlx_shape = (24,32)
 
-        except ValueError:
-            pass
-    
-    # Poll for socket connection
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    while (1):
+    def encode_frame(self, frame):
+        f = io.BytesIO()
+        np.savez(f, frame=frame)
+        
+        packet_size = len(f.getvalue())
+        header = '{0}:'.format(packet_size)
+        header = bytes(header.encode())  # prepend length of array
+
+        out = bytearray()
+        out += header
+        print(out)
+
+        f.seek(0)
+        out += f.read()
+        return out
+
+    def send(self, frame, socket):
+        if not isinstance(frame, np.ndarray):
+            raise TypeError("input frame is not a valid numpy array")
+
+        out = self.encode_frame(frame)
+
         try:
-            clientSocket.connect((server_addr, 2022))
-            break
-        except Exception as e:
-            pass
+            socket.sendall(out)
+        except BrokenPipeError:
+            print("connection broken")
+            raise
 
-    # Send sensor data to server
-    for frame in sensor_data:
-        for data in frame:
-            send(data, clientSocket)
+    def load_gps(self, path):
+        ''' Waypoint file format for parsing
+            QGC WPL <VERSION>
+            <INDEX> <CURRENT WP> <COORD FRAME> <COMMAND> <PARAM1> <PARAM2> <PARAM3> <PARAM4> <PARAM5/X/LATITUDE> <PARAM6/Y/LONGITUDE> <PARAM7/Z/ALTITUDE> <AUTOCONTINUE>
+        '''
+        file = open(path, "r")
+        file.readline() # Read QGC, WPL <Version>
+        file.readline() # Read home location information (not a waypoint)
+        path_data = file.readlines() # path_data contains all the waypoints
+        gps_coord = []
+        
+        # Parse GPS coordinates for all the waypoints
+        for wayPoint in path_data:
+            wayPoint = wayPoint.split('\t')
+            gps_coord.append((wayPoint[8], wayPoint[9]))
+
+        return gps_coord
+
+    def get_curr_gps(self):
+        return np.array([0,0])
     
-    # Close socket connection
-    clientSocket.close()
+    def flightDataCollection(self):
+        while(True):
+            sensor_data = self.collectPhotos()
+            self.startClient()
+            self.sendData(sensor_data)
+            self.clientSocket.close()
+
+    def collectPhotos(self):
+        sensor_data = []
+        gps_id = 0
+
+        # Collect data
+        while(len(sensor_data) < 3):
+            curr_coord = self.get_curr_gps() # Current GPS position recieved over telemetary port from drone
+            target_coord = self.gps_coordinates[gps_id]
+            time.sleep(5)
+            # Check to see if current position is within range of next waypoint
+            # if abs(curr_coord[0] - target_coord[0]) < 1.5 and abs(curr_coord[1] - target_coord[1]) < 1.5:
+            try:
+                curr_time = np.array([datetime.now()])
+                frame = np.zeros((24*32))
+                #mlx.getFrame(frame)
+                #frame = np.random.uniform(-20.0, 200.0, 32*24)
+                ir_data = (np.reshape(frame, self.mlx_shape))
+
+                # Get image data as numpy array
+                img_data = np.empty((240*320*3), dtype=np.uint8)
+
+                #camera.capture(img_data, 'bgr')
+                img_data = np.reshape(img_data, (240,320,3))
+                
+                sensor_data.append((ir_data, img_data, curr_coord, curr_time))
+                gps_id += 1
+
+            except ValueError:
+                pass
+
+            return sensor_data
+
+    def startClient(self):
+        # Poll for socket connection
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while (1):
+            try:
+                self.clientSocket.connect((self.server_addr, 2022))
+                break
+            except Exception as e:
+                pass
+
+    def sendData(self, sensor_data):
+        # Send sensor data to server
+        for frame in sensor_data:
+            for i, data in enumerate(frame):
+                print(f"sent {i}")
+                self.send(data, self.clientSocket)

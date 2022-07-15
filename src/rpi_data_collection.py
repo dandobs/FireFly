9a539aa584d0358691cbef56048148446e6a0b92
Daniel Arakcheev <darakche@uwaterloo.ca>
2022-07-14 20:27:40 -0400

    changed flag to debug

--- src/rpi_data_collection.py
+++ src/rpi_data_collection.py
@@ -8,10 +8,10 @@ import io
 import socket
 import time
 
-USING_RASPI = False
+DEBUG = False
 
 # imports for raspberry pi
-if USING_RASPI:
+if not DEBUG:
     import busio
     import adafruit_mlx90640
     from picamera import PiCamera
@@ -25,8 +25,9 @@ class DataCollector:
         self.gps_coordinates = self.load_gps(self.gps_path)
         self.num_pics = len(self.gps_coordinates)
 
-        #self.server_addr = "192.168.10.43"
-        self.server_addr = "127.0.0.1"
+        self.server_addr = "192.168.10.43"
+        if DEBUG:
+            self.server_addr = "127.0.0.1"
 
         self.setupSensors()
         self.flightDataCollection()
@@ -34,7 +35,7 @@ class DataCollector:
 
     def setupSensors(self):
         # Instantiate sensor modules & communication protocol
-        if USING_RASPI:
+        if not DEBUG:
             i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
             self.mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
             self.mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ # set refresh rate
@@ -105,7 +106,7 @@ class DataCollector:
         gps_id = 0
 
         # Collect data
-        while(len(sensor_data) < 3):
+        while(len(sensor_data) < self.num_pics if not DEBUG else 3):
             curr_coord = self.get_curr_gps() # Current GPS position recieved over telemetary port from drone
             target_coord = self.gps_coordinates[gps_id]
             time.sleep(5)
@@ -114,15 +115,21 @@ class DataCollector:
             try:
                 curr_time = np.array([datetime.now()])
                 frame = np.zeros((24*32))
-                #mlx.getFrame(frame)
-                #frame = np.random.uniform(-20.0, 200.0, 32*24)
+                
+                if DEBUG:
+                    frame = np.random.uniform(-20.0, 200.0, 32*24)
+                else:
+                    mlx.getFrame(frame)                    
+
                 ir_data = (np.reshape(frame, self.mlx_shape))
 
                 # Get image data as numpy array
                 img_data = np.empty((240*320*3), dtype=np.uint8)
 
-                #camera.capture(img_data, 'bgr')
-                img_data = np.reshape(img_data, (240,320,3))
+                if DEBUG:
+                    img_data = np.reshape(img_data, (240,320,3))
+                else:
+                    camera.capture(img_data, 'bgr')
                 
                 sensor_data.append((ir_data, img_data, curr_coord, curr_time))
                 gps_id += 1
@@ -130,7 +137,7 @@ class DataCollector:
             except ValueError:
                 pass
 
-            return sensor_data
+        return sensor_data
 
     def startClient(self):
         # Poll for socket connection
@@ -138,7 +145,7 @@ class DataCollector:
         while (1):
             try:
                 self.clientSocket.connect((self.server_addr, 2022))
-                break
+                return
             except Exception as e:
                 pass
 

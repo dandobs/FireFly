ed60eb1e45c1b2806299996d023e09181a1b26e7
Richmond Victorkumar <rvictorkumar@uwaterloo.ca>
2022-07-19 20:19:37 -0400

    Small changes

--- src/testing/telemetry_test.py
+++ src/testing/telemetry_test.py
@@ -1,27 +1,29 @@
 # Import DroneKit-Python
 from dronekit import connect, VehicleMode
 import dronekit_sitl
+import time
 
-simulator = True
+simulator = False
 
 if simulator:
     sitl = dronekit_sitl.start_default()
     connection_string = sitl.connection_string()
-    print(f"Connecting to vehicle on: {connection_string}")
     vehicle = connect(connection_string, wait_ready=True)
 else:
     # Serial port on rpi
     connection_string = '/dev/ttyAMA0'
-    print(f"Connecting to vehicle on: {connection_string}")
     vehicle = connect(connection_string, wait_ready=True, baud=57600)
 
+fileName = "gps_log.txt"
 
-while(True):
-    try:
-        print("[Lat, Long]", vehicle.location.global_frame.lat, vehicle.location.global_frame.lon)
-    
-    except KeyboardInterrupt:
-        vehicle.close()
-        if simulator:
-            sitl.stop()
-        break
+with open(fileName, "w") as file:
+    for i in range(100):
+        try:
+            # log = f"[GPS Coordinates]: {vehicle.location.global_frame.lat}, {vehicle.location.global_frame.lon} \n"
+            # file.write(log)
+            print(vehicle.battery)
+        except KeyboardInterrupt:
+            vehicle.close()
+            if simulator:
+                sitl.stop()
+            break

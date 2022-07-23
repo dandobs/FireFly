f489fb58d769ea00597fc57f73d3b522c5446402
Daniel Arakcheev <darakche@uwaterloo.ca>
2022-07-23 12:33:21 -0400

    last version of backend server

--- src/server_backend.py
+++ src/server_backend.py
@@ -1,22 +1,29 @@
-from flask import Flask
 import json
+from flask import Flask
 from flask import request
+import base64
+from io import BytesIO
+import io
+from PIL import Image
 
-from db_util import create_db_connection, execute_query, read_query, getFlightNum
 
-"""
-Hotspot statuses
-0: not viewed
-1: viewed
-2: attending (going to assess the fire)
-3: dismissed
-"""
+from db_util import (
+    create_db_connection,
+    execute_query,
+    read_query,
+    getFlightNum,
+    json_serial,
+)
 
 pw = "Fire2022!"
 sqlConn = create_db_connection("localhost", "root", pw, "firefly_db")
 statuses = ["not viewed", "viewed", "attending", "dismissed"]
 app = Flask(__name__)
-app.run()
+
+
+@app.route("/")
+def main():
+    return "we out here"
 
 
 @app.route("/getAllImages", methods=["GET"])
@@ -40,11 +47,44 @@ def getAllImages():
                 WHERE image_records.flightNum = %s;"""
     lastFlightNum = 1
     params = [lastFlightNum]
-    result = read_query(sqlConn, query, params, as_json=True)
+    data = read_query(sqlConn, query, params, as_json=True)
 
     # convert image paths to actual byte data
+    for i, record in enumerate(data):
+        irPath = record.pop("irImagePath")
+        rgbPath = record.pop("rgbImagePath")
 
-    return result if result else -1
+        with open(irPath, "rb") as f:
+            ir_bytes = f.read()
+            ir_b64 = base64.b64encode(ir_bytes).decode("utf8")
+
+        with open(rgbPath, "rb") as f:
+            rgb_bytes = f.read()
+            rgb_b64 = base64.b64encode(rgb_bytes).decode("utf8")
+
+        data[i]["irData"] = ir_b64
+        data[i]["rgbData"] = rgb_b64
+
+    json_data = json.dumps(data, default=json_serial)
+    return json_data if data else -1
 
 
-# getAllImages()
+app.run()
+
+
+# DEBUGGGGGING
+def encodedecodeImageTest():
+    irPath = r"C:\a_UNI\4A\Capstone\FireFly\server_hd\rgb_images\rgb_-0.9497796415476341_-0.26070763235767735__22-07-2022.png"
+
+    # encode
+    with open(irPath, "rb") as f:
+        ir_bytes = f.read()
+        ir_b64 = base64.b64encode(ir_bytes).decode("utf8")
+
+    # decode
+    img_bytes = base64.b64decode(ir_b64.encode("utf-8"))  # the line umar needs
+    img = Image.open(io.BytesIO(img_bytes))
+    img.save("newImage.png")
+
+
+# encodedecodeImageTest()
